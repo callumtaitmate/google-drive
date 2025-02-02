083505912f38f82e5fec95d96bbb64e5c4458242
callumtaitmate <callumtaitmate@gmail.com>
2025-02-02 21:04:29 +0000

    Schema fix

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -23,18 +23,15 @@ export const files_table = createTable(
     id: bigint("id", { mode: "number", unsigned: true })
       .primaryKey()
       .autoincrement(),
-    ownerId: text("owner_id").notNull(),
 
     name: text("name").notNull(),
     size: int("size").notNull(),
     url: text("url").notNull(),
     parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
   },
   (t) => {
     return [
       index("parent_index").on(t.parent),
-      index("owner_id_index").on(t.ownerId),
     ];
   },
 );
@@ -47,16 +44,13 @@ export const folders_table = createTable(
     id: bigint("id", { mode: "number", unsigned: true })
       .primaryKey()
       .autoincrement(),
-    ownerId: text("owner_id").notNull(),
 
     name: text("name").notNull(),
     parent: bigint("parent", { mode: "number", unsigned: true }),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
   },
   (t) => {
     return [
       index("parent_index").on(t.parent),
-      index("owner_id_index").on(t.ownerId),
     ];
   },
 );
