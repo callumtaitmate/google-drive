ac095f60051a41a8c466c26e055dfbec51d6c6e2
callumtaitmate <callumtaitmate@gmail.com>
2025-02-02 20:42:03 +0000

    Removed redundant mock instances

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -1,6 +1,4 @@
-import { mockFolders, mockFiles } from "../initialData";
-import { files, folders } from "~/server/db/schema"
-import { db } from "~/server/db";
+
 
 export default function SandboxPage() {
 
@@ -12,30 +10,7 @@ export default function SandboxPage() {
 
             <div>
                 <h2>Seed Function</h2>
-                <form action={async () => {
-                    "use server";
-
-
-                  // const folderInsert = await db.insert(folders).values(mockFolders.map((folder, index) => ({
-                    //    id: index + 1,
-                      //  name: folder.name,
-                        //parent: index !== 0 ? 1 : null ,
-                   //  })));
-                    const fileInsert = await db.insert(files).values(mockFiles.map((file, index) => ({
-
-                        id: index + 1,
-                        name: file.name,
-                        size: 500,
-                        url: file.url,
-                        parent: (index % 3) + 1
-
-                    })));
-
-                    console.log(fileInsert);
-                }}
-                >
-                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Seed</button>
-                </form>
+                
             </div>
         </div>
     )
