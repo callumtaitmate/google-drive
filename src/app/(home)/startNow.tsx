c7a5693f7ea382a32b2a85682bee58bee26c22d7
callumtaitmate <callumtaitmate@gmail.com>
2025-02-08 21:10:46 +0000

    Auth Added

--- src/app/(home)/startNow.tsx
+++ src/app/(home)/startNow.tsx
@@ -1,22 +1,21 @@
-import { auth } from "@clerk/nextjs/server";
 import { Button } from "../../components/ui/button"
 import { redirect } from "next/navigation";
 
 export default function StartNow() {
   return (
-    
+
     <form
-    action={async() => {
-        
+      action={async () => {
+
         return redirect("/drive");
 
-    }}
+      }}
     >
 
-    <Button className="mt-4 bg-gray-200 hover:bg-gray-300 text-gray-800" size="sm">
-          Start Now
-        </Button>
-        </form>
+      <Button className="mt-4 bg-gray-200 hover:bg-gray-300 text-gray-800" size="sm">
+        Start Now
+      </Button>
+    </form>
   )
 }
 
